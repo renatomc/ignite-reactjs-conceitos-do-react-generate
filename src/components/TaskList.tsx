e171c46a411df12315d293e72e821cb47c838851
Renato Martinez Costa <renatomc@hotmail.com>
2021-03-24 00:38:31 -0300

    Entrega do desafio

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import uuid from 'react-uuid'
 
 import '../styles/tasklist.scss'
 
@@ -16,14 +17,34 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+    const id = uuid();
+    const oldTasks = tasks;
+    const newTask = {
+      id,
+      title: newTaskTitle,
+      isComplete: false,
+    }
+    setTasks([...oldTasks, newTask]);
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const oldTasks = tasks;
+
+    const findIndex = oldTasks.findIndex(task => task.id === id);
+
+    if(findIndex < 0) return;
+
+    oldTasks[findIndex].isComplete = !oldTasks[findIndex].isComplete;
+
+    setTasks([...oldTasks]);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newTasks = tasks.filter(task => task.id !== id);
+    setTasks([...newTasks]);
   }
 
   return (
